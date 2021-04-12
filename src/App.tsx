d71c2931d704b44053beae1bc5d88989fb4e19fc
mayur0977 <mayur0977@users.noreply.github.com>
2021-04-12 22:06:55 +0530

    feat : added image svg webpack config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-
-
+import './styles.css';
+import IMAGE from './react.jpg'
+import SVG_LOGO from './410.svg';
 export const App = () => {
     return (
-        <h1>
-           React typescript webpack starter template 
+        <><h1>
+            React typescript webpack starter template 
         </h1>
+        <img src={IMAGE} alt="react logo"/>
+        <img src={SVG_LOGO} alt="react logo" height="200" width="200"/>
+        </>
     )
 }
